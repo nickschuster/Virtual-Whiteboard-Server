4cbacd8ebf7b2d5c11b348cbf3d98b8fb8597f23
Nick Schuster <ngpnickschster@gmail.com>
2020-11-17 15:47:39 -0800

    Added room history.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const SWITCH_EVENT = "switch_event"
 const DRAW_EVENT = "draw_event"
 const RENAME_EVENT = "rename_event"
 const DELETE_EVENT = "delete_event"
+const HISTORY_EVENT = "history_event"
 
 const fs = require("fs")
 const { connected, exit } = require("process")
@@ -28,8 +29,7 @@ console.log("VW Server Listening ... ")
 
 // Room globals.
 
-let activeCanvas;
-let mouseActivity;
+let roomHistory = [];
 let connectedClients = 0
 
 io.on('connect', socket => {
@@ -48,27 +48,37 @@ io.on('connect', socket => {
         if(type === HOST) {
         } else if(type === CLIENT) {
             socket.join(STUDENT_ROOM);
+            sendHistory(socket)
         }
     })
 
     socket.on(CREATE_EVENT, () => {
         socket.to(STUDENT_ROOM).emit(CREATE_EVENT);
+        addToHistory(CREATE_EVENT, undefined)
     })
 
     socket.on(SWITCH_EVENT, canvasId => {
         socket.to(STUDENT_ROOM).emit(SWITCH_EVENT, canvasId);
+        addToHistory(SWITCH_EVENT, canvasId)
     })
 
     socket.on(DRAW_EVENT, clickData => {
         socket.to(STUDENT_ROOM).emit(DRAW_EVENT, clickData);
+        addToHistory(DRAW_EVENT, clickData)
     })
 
     socket.on(RENAME_EVENT, renameDetails => {
         socket.to(STUDENT_ROOM).emit(RENAME_EVENT, renameDetails);
+        addToHistory(RENAME_EVENT, renameDetails)
     })
 
     socket.on(DELETE_EVENT, canvasId => {
         socket.to(STUDENT_ROOM).emit(DELETE_EVENT, canvasId);
+        addToHistory(DELETE_EVENT, canvasId)
+    })
+
+    socket.on(HISTORY_EVENT, () => {
+        sendHistory(socket)
     })
 
     socket.on("disconnect", reason => {
@@ -76,6 +86,18 @@ io.on('connect', socket => {
         connectedClients -= 1;
     })
 })
+
+// Save an event to the history.
+function addToHistory(event, details) {
+    roomHistory.push({type: event, detail: details})
+}
+
+// Send all current room history to the new joiner.
+function sendHistory(socket) {
+    for(const event of roomHistory) {
+        socket.emit(event.type, event.detail)
+    }
+}
 
 setInterval(() => {
     if(connectedClients === 0) {
